9abf0c90e9374dbf5f31510582b75deb298faea5
George Mitenkov <georgemitenk0v@gmail.com>
2021-08-10 13:46:36 +0300

    [LoopVectorize] Vectorize `bytecast`s

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -6834,6 +6834,9 @@ LoopVectorizationCostModel::getInstructionCost(Instruction *I,
     if (I->getType()->isPointerTy())
       return 0;
     [[fallthrough]];
+  case Instruction::ByteCast:
+    return 0;
+    [[fallthrough]];
   case Instruction::ZExt:
   case Instruction::SExt:
   case Instruction::FPToUI:
