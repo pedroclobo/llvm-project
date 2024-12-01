b6eff24b4b7c113c790e1c2215cca4d5aa01a115
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2024-12-01 13:20:16 +0000

    [InstCombine] Combine `load/bytecast` pairs

--- llvm/lib/Transforms/InstCombine/InstCombineLoadStoreAlloca.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineLoadStoreAlloca.cpp
@@ -720,6 +720,14 @@ static Instruction *combineLoadToOperationType(InstCombinerImpl &IC,
         return &Load;
       }
     }
+
+    if (auto *BC = dyn_cast<ByteCastInst>(Load.user_back())) {
+      Type *DestTy = BC->getDestTy();
+      LoadInst *NewLoad = IC.combineLoadToNewType(Load, DestTy);
+      BC->replaceAllUsesWith(NewLoad);
+      IC.eraseInstFromFunction(*BC);
+      return &Load;
+    }
   }
 
   // FIXME: We should also canonicalize loads of vectors when their elements are
