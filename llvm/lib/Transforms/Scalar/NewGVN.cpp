d84e466cdfb5b8c7f1e6a4d8330c97f89f68f4b4
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2025-01-27 11:21:15 +0000

    [NewGVN] Perform symbolic evaluation on `bytecast`

--- llvm/lib/Transforms/Scalar/NewGVN.cpp
+++ llvm/lib/Transforms/Scalar/NewGVN.cpp
@@ -2031,6 +2031,7 @@ NewGVN::performSymbolicEvaluation(Instruction *I,
   case Instruction::Load:
     E = performSymbolicLoadEvaluation(I);
     break;
+  case Instruction::ByteCast:
   case Instruction::BitCast:
   case Instruction::AddrSpaceCast:
   case Instruction::Freeze:
