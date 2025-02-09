9b5355cdf61296cca8f08adc011e24c7af680de9
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2025-02-09 11:34:49 +0000

    [VPlan] Convert byte values using `bytecast`

--- llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
@@ -3805,8 +3805,9 @@ void VPWidenStoreEVLRecipe::printRecipe(raw_ostream &O, const Twine &Indent,
 }
 #endif
 
-static Value *createBitOrPointerCast(IRBuilderBase &Builder, Value *V,
-                                     VectorType *DstVTy, const DataLayout &DL) {
+static Value *createBitOrByteOrPointerCast(IRBuilderBase &Builder, Value *V,
+                                           VectorType *DstVTy,
+                                           const DataLayout &DL) {
   // Verify that V is a vector type with same number of elements as DstVTy.
   auto VF = DstVTy->getElementCount();
   auto *SrcVecTy = cast<VectorType>(V->getType());
@@ -3820,6 +3821,13 @@ static Value *createBitOrPointerCast(IRBuilderBase &Builder, Value *V,
   if (CastInst::isBitOrNoopPointerCastable(SrcElemTy, DstElemTy, DL)) {
     return Builder.CreateBitOrPointerCast(V, DstVTy);
   }
+
+  if (SrcElemTy->isByteTy())
+    return Builder.CreateByteCast(V, DstVTy);
+
+  if (SrcElemTy->isPointerTy() && DstElemTy->isByteTy())
+    return Builder.CreateBitCast(V, DstVTy);
+
   // V cannot be directly casted to desired vector type.
   // May happen when V is a floating point vector but DstVTy is a vector of
   // pointers or vice-versa. Handle this using a two-step bitcast using an
@@ -3994,8 +4002,8 @@ void VPInterleaveRecipe::execute(VPTransformState &State) {
       // If this member has different type, cast the result type.
       if (Member->getType() != ScalarTy) {
         VectorType *OtherVTy = VectorType::get(Member->getType(), State.VF);
-        StridedVec =
-            createBitOrPointerCast(State.Builder, StridedVec, OtherVTy, DL);
+        StridedVec = createBitOrByteOrPointerCast(State.Builder, StridedVec,
+                                                  OtherVTy, DL);
       }
 
       if (Group->isReverse())
@@ -4040,7 +4048,8 @@ void VPInterleaveRecipe::execute(VPTransformState &State) {
     // If this member has different type, cast it to a unified type.
 
     if (StoredVec->getType() != SubVT)
-      StoredVec = createBitOrPointerCast(State.Builder, StoredVec, SubVT, DL);
+      StoredVec =
+          createBitOrByteOrPointerCast(State.Builder, StoredVec, SubVT, DL);
 
     StoredVecs.push_back(StoredVec);
   }
@@ -4157,8 +4166,8 @@ void VPInterleaveEVLRecipe::execute(VPTransformState &State) {
       // If this member has different type, cast the result type.
       if (Member->getType() != ScalarTy) {
         VectorType *OtherVTy = VectorType::get(Member->getType(), State.VF);
-        StridedVec =
-            createBitOrPointerCast(State.Builder, StridedVec, OtherVTy, DL);
+        StridedVec = createBitOrByteOrPointerCast(State.Builder, StridedVec,
+                                                  OtherVTy, DL);
       }
 
       State.set(getVPValue(J), StridedVec);
@@ -4185,7 +4194,8 @@ void VPInterleaveEVLRecipe::execute(VPTransformState &State) {
     Value *StoredVec = State.get(StoredValues[StoredIdx]);
     // If this member has different type, cast it to a unified type.
     if (StoredVec->getType() != SubVT)
-      StoredVec = createBitOrPointerCast(State.Builder, StoredVec, SubVT, DL);
+      StoredVec =
+          createBitOrByteOrPointerCast(State.Builder, StoredVec, SubVT, DL);
 
     StoredVecs.push_back(StoredVec);
     ++StoredIdx;
