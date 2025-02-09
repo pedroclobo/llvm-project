76d378fa9b5431b5c03298abf2c6e5a221f5f18a
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2025-02-09 11:34:49 +0000

    [VPlan] Create `bytecast`s when converting from bytes

--- llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
@@ -3512,8 +3512,9 @@ void VPWidenStoreEVLRecipe::print(raw_ostream &O, const Twine &Indent,
 }
 #endif
 
-static Value *createBitOrPointerCast(IRBuilderBase &Builder, Value *V,
-                                     VectorType *DstVTy, const DataLayout &DL) {
+static Value *createBitOrByteOrPointerCast(IRBuilderBase &Builder, Value *V,
+                                           VectorType *DstVTy,
+                                           const DataLayout &DL) {
   // Verify that V is a vector type with same number of elements as DstVTy.
   auto VF = DstVTy->getElementCount();
   auto *SrcVecTy = cast<VectorType>(V->getType());
@@ -3527,6 +3528,13 @@ static Value *createBitOrPointerCast(IRBuilderBase &Builder, Value *V,
   if (CastInst::isBitOrNoopPointerCastable(SrcElemTy, DstElemTy, DL)) {
     return Builder.CreateBitOrPointerCast(V, DstVTy);
   }
+
+  if (SrcElemTy->isByteTy())
+    return Builder.CreateExactByteCast(V, DstVTy);
+
+  if (SrcElemTy->isPointerTy() && DstElemTy->isByteTy())
+    return Builder.CreateBitCast(V, DstVTy);
+
   // V cannot be directly casted to desired vector type.
   // May happen when V is a floating point vector but DstVTy is a vector of
   // pointers or vice-versa. Handle this using a two-step bitcast using an
@@ -3702,7 +3710,8 @@ void VPInterleaveRecipe::execute(VPTransformState &State) {
       if (Member->getType() != ScalarTy) {
         VectorType *OtherVTy = VectorType::get(Member->getType(), State.VF);
         StridedVec =
-            createBitOrPointerCast(State.Builder, StridedVec, OtherVTy, DL);
+            createBitOrByteOrPointerCast(State.Builder, StridedVec, OtherVTy,
+                                         DL);
       }
 
       if (Group->isReverse())
@@ -3747,7 +3756,8 @@ void VPInterleaveRecipe::execute(VPTransformState &State) {
     // If this member has different type, cast it to a unified type.
 
     if (StoredVec->getType() != SubVT)
-      StoredVec = createBitOrPointerCast(State.Builder, StoredVec, SubVT, DL);
+      StoredVec = createBitOrByteOrPointerCast(State.Builder, StoredVec, SubVT,
+                                               DL);
 
     StoredVecs.push_back(StoredVec);
   }
